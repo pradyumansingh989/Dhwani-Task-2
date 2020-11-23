3f895b42182cc7a020e613644be4d46302d997dc
pradyumansingh989 <66694735+pradyumansingh989@users.noreply.github.com>
2020-11-23 17:28:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const div1 = document.getElementById("div1");
+const div2 = document.getElementById("div2");
+const div3 = document.getElementById("div3");
+const div4 = document.getElementById("div4");
+
+div1.addEventListener("click",()=>{
+    div1.style.zIndex = 1;
+});
+div2.addEventListener("click",()=>{
+    div2.style.zIndex = 1;
+});
+div3.addEventListener("click",()=>{
+    div3.style.zIndex = 1;
+});
+div4.addEventListener("click",()=>{
+    div4.style.zIndex = 1;
+});
